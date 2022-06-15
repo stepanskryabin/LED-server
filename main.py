6167f525410e74f2723734d302409cc3405a760e
Stepan Skriabin <stepan.skrjabin@gmail.com>
2022-06-15 19:59:53 +0300

    feat(main): add base main.py with get and post methods

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
+from enum import Enum
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
-from src.settings import ABOUT
-from src.help_desk.views import get_support_page
-from src.help_desk.views import post_support_page
+from src.db.worker import DBWorker
+
+app = FastAPI()
+
+db = DBWorker()
 
 
-app = FastAPI(description=ABOUT["description"],
-              version=ABOUT["version"],
-              contact=ABOUT["contact"])
+class Item(BaseModel):
+    user_name: str
+    date_time: int
+    time_zone: str
+    email: str
+    message: str
+    importance: int = 0
+
+
+class Model(str, Enum):
+    first = "first"
+    second = "second"
 
 
 @app.get("/")
@@ -17,21 +32,43 @@ async def get_root(skip: int = 0, limit: int = 10):
     return f"Hello World skip={skip}, limit={limit}"
 
 
-@app.put("/")
-async def put_root():
-    return None
+@app.on_event("startup")
+def on_startup():
+    db.connect()
+    db.create_record()
 
 
-@app.post("/")
-async def post_root():
-    return None
+@app.on_event("shutdown")
+def on_shutdown():
+    db.disconnect()
+
+
+@app.get("/login")
+async def login():
+    return {"Log In": "not implemented"}
+
+
+@app.get("/signin")
+async def signin():
+    return {"Sign In": "not implemented"}
+
+
+@app.post("/support/")
+async def post_support(item: Item):
+    db.add(user_name=item.user_name,
+           date_time=item.date_time,
+           time_zone=item.time_zone,
+           email=item.email,
+           message=item.message,
+           importance=item.importance)
+    return item
 
 
 @app.get("/support/{item}")
-async def get_support(item: int):
-    return await get_support_page(_input=item)
-
-
-@app.put("/support/{item}")
-async def put_support(item):
-    return await post_support_page(_input=item)
+async def get_support(item: Model, id: int):
+    if item == Model.first and id <= 10:
+        return db.get_by_id(id)
+    elif item == Model.second and id <= 10:
+        return {"result": "Second model"}
+    else:
+        return {"result": f"ID must be <= 10, not={id}"}
