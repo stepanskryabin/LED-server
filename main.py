a9903242b05039bf1a1f3834d6078bcae8f6bef2
Stepan Skriabin <stepan.skrjabin@gmail.com>
2022-07-13 12:11:47 +0300

    added response model

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from enum import Enum
-
-from fastapi import FastAPI, Query, status, HTTPException, Depends
+from fastapi import FastAPI, Query, status, Depends
 from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy.exc import MultipleResultsFound
 
 from src.db.worker import DBWorker
-from src.schemas.schemas import UserLogin, UserRegister, UserRequest, UserResponse
+from src.schemas.schemas import ErrorResponse
+from src.schemas.schemas import UserAuth
+from src.schemas.schemas import UserLogin
+from src.schemas.schemas import InfoResponse
+from src.schemas.schemas import UserRegister
+from src.schemas.schemas import UserResponse
+from src.schemas.schemas import UserRequest
 from src.settings import ABOUT, origins
 
 app = FastAPI()
+
+additional_responses = {
+    401: {"model": ErrorResponse},
+    404: {"model": ErrorResponse},
+    500: {"model": ErrorResponse}
+    }
 
 app.add_middleware(
     CORSMiddleware,
@@ -21,13 +30,9 @@ app.add_middleware(
 db = DBWorker()
 
 
-class Model(str, Enum):
-    first = "first"
-    second = "second"
-
-
 @app.get("/", tags=['main'])
-async def get_root(skip: int = 0, limit: int = 10):
+async def get_root(skip: int = 0,
+                   limit: int = 10):
     if limit > 100:
         return "Error!"
     return f"Hello World skip={skip}, limit={limit}"
@@ -46,6 +51,8 @@ def on_shutdown():
 
 @app.post("/login",
           status_code=status.HTTP_202_ACCEPTED,
+          response_model=UserAuth,
+          responses=additional_responses,
           tags=['auth'])
 async def login(old_user: UserLogin):
     """
@@ -53,14 +60,16 @@ async def login(old_user: UserLogin):
     """
 
     if old_user.password == db.get_user(name=old_user.name).password:
-        auth_id = "auth_id"
-        return {"auth_id": auth_id}
-
-    return {"Login": "User unknown!"}
+        return UserAuth(auth_id="auth_id")
+    else:
+        return ErrorResponse(code=status.HTTP_401_UNAUTHORIZED,
+                         detail="User is unknow")
 
 
 @app.post("/signin",
           status_code=status.HTTP_201_CREATED,
+          response_model=InfoResponse,
+          responses=additional_responses,
           tags=['auth'])
 async def signin(new_user: UserRegister):
     """
@@ -70,13 +79,17 @@ async def signin(new_user: UserRegister):
     try:
         db.add_user(new_user)
     except Exception:
-        return {"Sign In": "Unknown error"}
+        raise ErrorResponse(code=status.HTTP_409_CONFLICT,
+                        detail="Unknow error")
     else:
-        return {"Sign In": "Ok"}
+        return InfoResponse(code=status.HTTP_201_CREATED,
+                            detail="User is created")
 
 
 @app.get("/user",
+         status_code=status.HTTP_200_OK,
          response_model=UserResponse,
+         responses=additional_responses,
          tags=['admin_panel'])
 async def userlist(username: str = Query(default=None,
                                          max_length=50,
@@ -86,11 +99,10 @@ async def userlist(username: str = Query(default=None,
     Search user.
     """
 
-    try:
-        result = db.get_user(name=username)
-    except MultipleResultsFound:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                            detail="BlaBlaBla")
+    result = db.get_user(name=username)
+    if result.name is None:
+        raise ErrorResponse(code=status.HTTP_404_NOT_FOUND,
+                        detail="User not found")
     else:
         return result
 
@@ -98,11 +110,17 @@ async def userlist(username: str = Query(default=None,
 @app.post("/user",
           response_model=UserResponse,
           status_code=status.HTTP_201_CREATED,
+          responses=additional_responses,
           tags=['admin_panel'])
 async def add_user(item: UserRequest):
     """
     Add new user.
     """
 
-    db.add_user(item)
-    return item
+    try:
+        db.add_user(item)
+    except Exception:
+        raise ErrorResponse(code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                        detail="Unknow server error")
+    else:
+        return item
--- src/schemas/schemas.py
+++ src/schemas/schemas.py
@@ -3,47 +3,110 @@ from pydantic import Field
 from pydantic import EmailStr
 
 
-class User(BaseModel):
-    name: str = Field(default=None,
-                      title="User name",
-                      max_length=50)
+__all__ = ("User",
+           "UserResponse",
+           "UserRequest",
+           "UserLogin",
+           "UserRegister",
+           "UserError",
+           "InfoResponse")
+
+
+# Hidden class, not used for work
+class _UserLogin(BaseModel):
     login: str = Field(default=None,
                        title="User login name",
                        max_length=50)
+
+
+class _UserName(BaseModel):
+    name: str = Field(default=None,
+                      title="User name",
+                      max_length=50)
+
+
+class _UserPassword(BaseModel):
     password: str = Field(default=None,
                           title="User password",
                           max_length=50)
+
+
+class _UserAuth(BaseModel):
+    auth_id: str = Field(default=None,
+                         title='authentication token')
+
+
+class _UserEmail(BaseModel):
     email: EmailStr = Field(default=None,
                             title="Contact email")
+
+
+class _UserIsDeleted(BaseModel):
     is_deleted: bool = Field(default=False,
                              title="status of the account")
+
+
+class _UserIsActivated(BaseModel):
     is_activated: bool = Field(default=False,
                                title="activation status of the account")
 
 
-class UserResponse(User):
+class _UserIsCreated(BaseModel):
     is_created: bool = Field(default=False,
                                title="create or not")
+
+
+class _UserError(BaseModel):
+    code: int =Field(default=None, title="Status code")
+    detail: str = Field(default=None, title="Message text")
+
+
+# Working schemas classes
+class User(_UserName,
+           _UserLogin,
+           _UserEmail,
+           _UserIsDeleted,
+           _UserIsActivated):
+    pass
+
+
+class UserResponse(_UserName,
+                   _UserLogin,
+                   _UserEmail,
+                   _UserIsDeleted,
+                   _UserIsActivated,
+                   _UserIsCreated):
 
     class Config:
         orm_mode = True
 
 
-class UserRequest(User):
-    auth_id: str = Field(default=None,
-                         title='authentication token')
+class UserRequest(_UserName,
+                   _UserLogin,
+                   _UserEmail,
+                   _UserIsDeleted,
+                   _UserIsActivated,
+                   _UserAuth):
+    pass
 
 
-
-class UserLogin(BaseModel):
-    name: str = Field(default=None,
-                      title="Unique user name",
-                      max_length=50)
-    password: str = Field(default=None,
-                          title="User password",
-                          max_length=50)
+class UserLogin(_UserName,
+                _UserPassword):
+    pass
 
 
-class UserRegister(UserLogin):
-    email: EmailStr = Field(default=None,
-                            title="Contact email")
+class UserRegister(_UserName,
+                   _UserPassword,
+                   _UserEmail):
+    pass
+
+class UserAuth(_UserAuth):
+    pass
+
+
+class ErrorResponse(_UserError):
+    pass
+
+
+class InfoResponse(_UserError):
+    pass
