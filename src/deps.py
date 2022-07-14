6d6007bd759b88ce9dfe45233c75b58001e69c49
Stepan Skriabin <stepan.skrjabin@gmail.com>
2022-07-14 14:47:33 +0300

    add dependenses

--- src/deps.py
+++ src/deps.py
@@ -1,19 +1,28 @@
-from mimetypes import init
+import heapq
+
 from typing import Any
 
 
-class Singleton(type):
-    _instances = {}
+# class Singleton(type):
+#     _instances = {}
 
-    def __call__(cls, *args: Any, **kwargs: Any) -> Any:
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
+#     def __call__(cls, *args: Any, **kwargs: Any) -> Any:
+#         if cls not in cls._instances:
+#             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+#         return cls._instances[cls]
 
 
-class AuthSession(metaclass=Singleton):
-    def __init__(self) -> None:
-        self.session = []
+# class SessionQueue(metaclass=Singleton):
+#     def __init__(self) -> None:
+#         self.queue = []
+#         heapq.heapify(self.queue)
+
+#     async def pull(self,
+#                    item: Any) -> None:
+#         await heapq.heappush(self.queue, item)
+
+#     async def push(self) -> Any:
+#         return await heapq.heappop(self.queue)
 
 
 async def check_auth_id(auth_id: str):
