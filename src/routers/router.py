b6cabb376ab221687920bcf03c51a69d84d6b066
Stepan Skriabin <stepan.skrjabin@gmail.com>
2022-08-05 11:51:48 +0300

    fixed: delete dependencies

--- src/routers/router.py
+++ src/routers/router.py
@@ -1,6 +1,5 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 
-from src.core.deps import check_auth_token
 from src.routers import auth
 from src.routers import admin_panel
 from src.routers import home
@@ -10,6 +9,5 @@ api_router = APIRouter()
 api_router.include_router(auth.router,
                           prefix="/auth")
 api_router.include_router(admin_panel.router,
-                          prefix="/admin",
-                          dependencies=[Depends(check_auth_token)])
+                          prefix="/admin")
 api_router.include_router(home.router)
