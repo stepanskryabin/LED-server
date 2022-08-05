a5846b7e4c67d3ec4c0fe9deef8838f6e8ebf90d
Stepan Skriabin <stepan.skrjabin@gmail.com>
2022-08-05 11:52:54 +0300

    update test

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -1,15 +1,21 @@
+from src.db.worker import DBWorker
 
 
 class TestDatabase:
-    def test_get_by_name(self, db):
-        result = db.get_user(name="Peter")
+    def test_get_by_name(self, fx_db):
+        result = fx_db.get_user(name="Test")
         assert result.is_created is True
 
-    def test_get_by_name_2(self, db):
-        result = db.get_user(name="Wrong")
+    def test_get_by_name_2(self, fx_db):
+        result = fx_db.get_user(name="Wrong")
         assert result.is_created is False
         assert result.name is None
 
-    def test_get_by_id(self, db):
-        result = db.get_user(_id=1)
+    def test_get_by_id(self, fx_db):
+        result = fx_db.get_user(_id=1)
+        assert result.is_created is True
+
+    def test_one_connection(self, fx_db):
+        db = DBWorker()
+        result = db.get_user(name="Test")
         assert result.is_created is True
