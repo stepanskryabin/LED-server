11e13ff838e19aa2e701db72c2639df2858d9969
Stepan Skriabin <stepan.skrjabin@gmail.com>
2022-08-05 11:49:00 +0300

    fixed some bugs

--- src/core/deps.py
+++ src/core/deps.py
@@ -1,14 +1,14 @@
 from typing import Any
-
-from fastapi import Depends, Header
+from fastapi import Header
 from fastapi import HTTPException
 from fastapi import status
 from jose import JWTError, jwt
 
-from src.core.security import oauth2_scheme
+from src.core.security import create_access_token
 from src.core.security import SECRET_KEY
 from src.core.security import ALGORITHM
 from src.db.worker import DBWorker
+from src.schemas.schemas import UserLogin
 
 
 db = DBWorker()
@@ -24,26 +24,10 @@ class CredentialsError(HTTPException):
                          headers=headers)
 
 
-def get_auth_token(token: str = Depends(oauth2_scheme)) -> None:
-    payload: dict
-    username: str
-
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-    except JWTError:
-        raise CredentialsError
-    else:
-        username = payload.get("sub")
-        if username is None:
-            raise CredentialsError
-
-    user = db.get_user(name=username)
-    if user.name is None:
-        raise CredentialsError
-
-    if not user.is_activated:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail="Inactive user")
+def get_auth_token(user: UserLogin) -> str:
+    token = create_access_token(data=user.dict(),
+                                expires_delta=30)
+    return token
 
 
 def check_auth_token(token: str = Header()) -> None:
--- tests/test_deps.py
+++ tests/test_deps.py
@@ -1,27 +1,48 @@
 from unittest.mock import patch
 
+import pytest
 from jose import jwt
 
+from src.core.deps import CredentialsError
 from src.core.deps import get_auth_token
 from src.db.worker import DBWorker
 from src.core.deps import check_auth_token
+from src.schemas.schemas import UserDBResult
 
 
-class TestAuthToken:
-    @patch.object(DBWorker, 'get_user')
-    @patch.object(jwt, 'decode')
-    @patch('fastapi.Depends')
-    def test_get_auth_token(self, _, mock_decode, mock_db):
-        result = get_auth_token(token="test")
-        assert mock_decode.call_count == 1
-        assert mock_db.call_count == 1
-        assert result is None
+class TestDeps:
+    class TestGetAuthToken:
+        def test_get_auth_token(self, fx_user):
+            result = get_auth_token(user=fx_user)
+            assert isinstance(result, str)
 
-    @patch.object(DBWorker, 'get_user')
-    @patch.object(jwt, 'decode')
-    @patch('fastapi.Header')
-    def test_check_auth_token(self, _, mock_decode, mock_db):
-        result = check_auth_token(token="test")
-        assert mock_decode.call_count == 1
-        assert mock_db.call_count == 1
-        assert result is None
+    class TestCheckAuthToken:
+        @patch.object(DBWorker, 'get_user')
+        @patch.object(jwt, 'decode')
+        def test_unit(self, mock_decode, mock_db):
+            result = check_auth_token(token="test")
+            assert mock_decode.call_count == 1
+            assert mock_db.call_count == 1
+            assert result is None
+
+        def test_error_jwt_decode(self):
+            with pytest.raises(CredentialsError):
+                check_auth_token(token="test")
+
+        @patch.object(jwt, 'decode')
+        @patch.object(DBWorker,
+                      'get_user',
+                      return_value=UserDBResult())
+        def test_error_db_user_is_none(self, mock_db, mock_decode):
+            _ = mock_db, mock_decode
+            with pytest.raises(CredentialsError):
+                check_auth_token(token="test")
+
+        @patch.object(jwt, 'decode')
+        @patch.object(DBWorker,
+                      'get_user',
+                      return_value=UserDBResult(is_activated=False))
+        def test_error_db_user_is_not_activated(self, mock_db, mock_decode):
+            _ = mock_db, mock_decode
+            with pytest.raises(CredentialsError):
+                check_auth_token(token="test")
